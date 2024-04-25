0776ef248e18a15d8f68dba8dd14c43abdb9b71e
Jason <jlee55504@gmail.com>
2024-04-25 13:32:14 -0700

    Updated the 'RecipeList.js' 'file'. I mistakenly committed and pushed out-dated code. This code is the most recent.

--- Project-_Recipe_App_-React_18--Jason_L.-Solution/src/RecipeList.js
+++ Project-_Recipe_App_-React_18--Jason_L.-Solution/src/RecipeList.js
@@ -1,6 +1,5 @@
 /* 'Imports' "React" from 'react'. */
 import React from "react";
-import './RecipeList.css';
 /* The "RecipeList" 'function/component' takes two 'parameters'; "recipes" (a 
     'variable/array' from './App.js') and the "deleteRecipe" (a 'function' from 
 './App.js'). The 'function' "getIndexToDelete" 'function' inside the "RecipeList"
@@ -14,7 +13,7 @@ import './RecipeList.css';
   "recipes" 'variable/array'. A 'div' JSX 'element' is returned with a 'table', 
   a 'thead', a 'tr' and a 'tbody' JSX 'element(s)' with all the 'data' from the 
   "createRecipeTableData" 'variable' inside of the 'tbody' JSX 'element'. */
-function RecipeList({ recipes, getRecipes }) {
+function RecipeList({ recipes, deleteRecipe }) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
@@ -24,34 +23,34 @@ function RecipeList({ recipes, getRecipes }) {
   'function/component' takes one 'parameter' named "index" and 'calls' the 
  "deleteRecipes" function with the value of the "index" 'parameter' using the 
  'target' and 'value' 'methods'. */
-  /*const getIndexToDelete = index => {
+  const getIndexToDelete = index => {
     deleteRecipe(index.target.value)
-  }*/
+  }
 
     /* The "createRecipeTableData" 'variable' uses the '.map' 'method' on the 
 "recipes" 'parameter' ('array') to create a 'tr' JSX 'element' containing six 
 'td' JSX 'elements'. The first, second, fourth and fifth 'td' JSX 'elements' 
-contain 'p' JSX 'elements' inside. The third 'td' JSX 'element' contains a 
+contain 'div' JSX 'elements' inside. The third 'td' JSX 'element' contains a 
 'img' JSX 'element' with the 'value' of the current item in the "recipes" 
 'parameter's' ('array') "photo" 'key' and the value, the current item in the 
 "recipes" 'parameter's' ('array') "name" 'key' as the 'value' for its 'alt' 
 'attribute'. The sixth 'td' JSX 'element' contains a 'button' JSX 'element' with
  the 'value' of "button" for its 'role' 'attribute', the 'value' of "delete" for 
  its 'name' 'attribute', the 'text' "Delete" and the "getIndexToDelete" 'function'
-  as the 'value' for its 'onChange' 'event listener'. The first 'p' JSX 'element'
+  as the 'value' for its 'onChange' 'event listener'. The first 'div' JSX 'element'
    inside of the first 'td' JSX 'element' contains the 'value' of the current 
-   item's "name" 'key' for its 'text'. The second 'p' JSX 'element' inside of the
+   item's "name" 'key' for its 'text'. The second 'div' JSX 'element' inside of the
     second 'td' JSX 'element' contains the 'value' of the current item's "cuisine"
-     'key' for its 'text'. The fourth 'p' JSX 'element' inside of the fourth 'td'
+     'key' for its 'text'. The fourth 'div' JSX 'element' inside of the fourth 'td'
       JSX 'element' contains the 'value' of the current item's "ingredients" 'key'
-       for its 'text'. The fifth 'p' JSX 'element' inside of the fifth 'td' JSX 
+       for its 'text'. The fifth 'div' JSX 'element' inside of the fifth 'td' JSX 
        'element' contains the 'value' of the current item's "preparation" 'key' for
         its 'text'. */
-    const createRecipeTableData = recipes.map((recipe, index) => <tr key={index}><td><p>{recipe.name}</p></td><td><p>{recipe.cuisine}</p></td><td><img src={recipe.photo} alt={recipe.name} /></td>
-    <td><div>{recipe.ingredients}</div></td><td><p>{recipe.preparation}</p></td><td><button type="button" name="delete" onClick={getIndexToDelete} value={index}>Delete</button></td> </tr>);
+        const createRecipeTableData = recipes.map((recipe, index) => <tr key={index}><td><div>{recipe.name}</div></td><td><div>{recipe.cuisine}</div></td><td><img src={recipe.photo} alt={recipe.name} /></td>
+    <td><div className="ingredients-div">{recipe.ingredients}</div></td><td><div className="preparation-div">{recipe.preparation}</div></td><td><button type="button" name="delete" onClick={getIndexToDelete} value={index}>Delete</button></td> </tr>);
 
 
-getRecipes(createRecipeTableData);
+
 
 
   /* A 'div' JSX 'element' with the 'value' of "recipe-list" for its 'className' 
@@ -59,7 +58,7 @@ getRecipes(createRecipeTableData);
    'th' JSX 'elements' ("Name", "Cuisine", "Photo", "ingredients", "Preparation" 
 and "Actions" for their 'text'). After the 'thead' JSX 'element comes a 'tbody' 
 JSX 'element' with all the 'data' from the "createRecipeTableData" 'variable'. */  
- /* return (
+ return (
     <div className="recipe-list">
       <table>
         <thead>
@@ -67,8 +66,8 @@ JSX 'element' with all the 'data' from the "createRecipeTableData" 'variable'. *
             <th>Name</th>
             <th>Cuisine</th>
             <th>Photo</th>
-            <th className="ingredients">Ingredients</th>
-            <th className="preparation">Preparation</th>
+            <th className="ingredients-th">Ingredients</th>
+            <th className="preparation-th">Preparation</th>
             <th>Actions</th>
           </tr>
         </thead>
@@ -77,7 +76,9 @@ JSX 'element' with all the 'data' from the "createRecipeTableData" 'variable'. *
         </tbody>
       </table>
     </div>
-  );*/
+  );
 }
 /* 'Exports' the "RecipeList" 'function/component'. */
 export default RecipeList;
+
+
